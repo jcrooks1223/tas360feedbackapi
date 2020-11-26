3e4575af637327eca93b8699bb10d66fcd5626a7
jcrooks1223 <60272432+jcrooks1223@users.noreply.github.com>
2020-11-25 21:55:17 -0600

    Update API

--- Models/ReadFeedbackData.cs
+++ Models/ReadFeedbackData.cs
@@ -104,63 +104,65 @@ namespace API.Models
             // return allSurveys;
         }
 
-        // public Survey GetSurvey(int id)
-        // {
-        //     // string cs = @"URI=file:C:\source\repos\TAS-360-Feedback\TAS\feedback1.db";
-        //     // using var con = new SQLiteConnection(cs);
-        //     // con.Open();
-        //     List<Survey> allSurvey = new List<Survey>();
-        //     DBConnect db = new DBConnect();
-        //     bool isOpen = db.OpenConnection();
-        //     if(isOpen)
-        //     {
-        //      MySqlConnection conn = db.GetConn();
-        //      string stm = "SELECT * FROM feedback WHERE SurveyID=@SurveyID";
-        //      MySqlCommand cmd = new MySqlCommand(stm,conn);
+        public Survey GetSurvey(int id)
+        {
+            // string cs = @"URI=file:C:\source\repos\TAS-360-Feedback\TAS\feedback1.db";
+            // using var con = new SQLiteConnection(cs);
+            // con.Open();
+            
+            DBConnect db = new DBConnect();
+            bool isOpen = db.OpenConnection();
+            if(isOpen)
+            {
+             MySqlConnection conn = db.GetConn();
+             string stm = "SELECT * FROM feedback WHERE SurveyID=@SurveyID";
+             MySqlCommand cmd = new MySqlCommand(stm,conn);
 
-        //      cmd.Connection = conn;
-        //      cmd.CommandText = @"SELECT * FROM feedback WHERE SurveyID=@SurveyID";
-        //      cmd.Parameters.AddWithValue("@SurveyID", id);
-        //      cmd.Prepare();
-        //      //cmd.ExecuteNonQuery();
+             cmd.Connection = conn;
+             cmd.CommandText = @"SELECT * FROM feedback WHERE SurveyID=@SurveyID";
+             cmd.Parameters.AddWithValue("@SurveyID", id);
+             cmd.Prepare();
+             //cmd.ExecuteNonQuery();
              
 
-        //      using(var rdr = cmd.ExecuteReader())
-        //      {
-        //        rdr.Read();
+             using(var rdr = cmd.ExecuteReader())
+             {
+               rdr.Read();
+               db.CloseConnection();
                 
-        //           return new Survey(){SurveyID=rdr.GetInt32(0), 
-        //                                     Employee_Reviewing = rdr.GetString(1),
-        //                                     Employee_Feedback=rdr.GetString(2),
-        //                                     Lead1=rdr.GetInt32(3),
-        //                                     Lead2=rdr.GetInt32(4),
-        //                                     Lead3=rdr.GetInt32(5),
-        //                                     Lead4=rdr.GetInt32(6),
-        //                                     Lead5=rdr.GetInt32(7),
-        //                                     Comm1=rdr.GetInt32(8),
-        //                                     Comm2=rdr.GetInt32(9),
-        //                                     Comm3=rdr.GetInt32(10),
-        //                                     Comm4=rdr.GetInt32(11),
-        //                                     Comm5=rdr.GetInt32(12),
-        //                                     Tech1=rdr.GetInt32(13),
-        //                                     Tech2=rdr.GetInt32(14),
-        //                                     Tech3=rdr.GetInt32(15),
-        //                                     Tech4=rdr.GetInt32(16),
-        //                                     Tech5=rdr.GetInt32(17),
-        //                                     LeadTotal=rdr.GetInt32(18), 
-        //                                     CommTotal=rdr.GetInt32(19),
-        //                                     TechTotal=rdr.GetInt32(20), 
-        //                                     OverallScore=rdr.GetInt32(21),
-        //                                     SurveyDate=rdr.GetDateTime(22)};    
-        //      }
-        //     }
+                  return new Survey(){SurveyID=rdr.GetInt32(0), 
+                                            Employee_Reviewing = rdr.GetString(1),
+                                            Employee_Feedback=rdr.GetString(2),
+                                            Lead1=rdr.GetInt32(3),
+                                            Lead2=rdr.GetInt32(4),
+                                            Lead3=rdr.GetInt32(5),
+                                            Lead4=rdr.GetInt32(6),
+                                            Lead5=rdr.GetInt32(7),
+                                            Comm1=rdr.GetInt32(8),
+                                            Comm2=rdr.GetInt32(9),
+                                            Comm3=rdr.GetInt32(10),
+                                            Comm4=rdr.GetInt32(11),
+                                            Comm5=rdr.GetInt32(12),
+                                            Tech1=rdr.GetInt32(13),
+                                            Tech2=rdr.GetInt32(14),
+                                            Tech3=rdr.GetInt32(15),
+                                            Tech4=rdr.GetInt32(16),
+                                            Tech5=rdr.GetInt32(17),
+                                            LeadTotal=rdr.GetInt32(18), 
+                                            CommTotal=rdr.GetInt32(19),
+                                            TechTotal=rdr.GetInt32(20), 
+                                            OverallScore=rdr.GetInt32(21),
+                                            SurveyDate=rdr.GetDateTime(22)};   
+             }
+             
+            }
             
-        //     else
-        //     {
-        //         return new Survey();
-        //     }
-           
-        // }
+            else
+            {
+                return new Survey();
+            }
+            
+        }
         
     }
 }
